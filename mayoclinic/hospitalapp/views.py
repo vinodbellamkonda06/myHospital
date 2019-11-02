381188af87c89250eacf620f8f68e70ec5e8002a
vinod <bkvinod477@gmail.com>
2019-11-02 15:54:47 +0530

    added urls.py file

--- mayoclinic/hospitalapp/views.py
+++ mayoclinic/hospitalapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def test(request):
+    """ view created for testing the modification 
+        it's just a sample view
+    """
+    return HttpResponse("welcome")
